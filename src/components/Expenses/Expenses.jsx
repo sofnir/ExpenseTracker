3b90c185c93a1c6ad94cac74a1ec76052e8ee6c6
Maciej Gwiżdż <mgwizdz@future-processing.com>
2021-12-23 15:22:59 +0100

    Create ExpenseFilter component.

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,17 +1,29 @@
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
+import ExpensesFilter from '../ExpensesFilter/ExpensesFilter';
+import { useState } from 'react';
 
 const Expenses = ({ expenses }) => {
+  const [_yearFilter, setYearFilter] = useState('2020');
+
+  const filterHandler = year => {
+    setYearFilter(year);
+    console.log(year);
+  };
+
   return (
     <Card className='expenses'>
-      {expenses.map((expense, index) => {
-        return <ExpenseItem
-          key={index}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date} />
-      })}
+      <ExpensesFilter selected={_yearFilter} onFilter={filterHandler} />
+      {expenses
+        .filter(expense => expense.date.getFullYear() == _yearFilter)
+        .map((expense, index) => {
+          return <ExpenseItem
+            key={index}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date} />
+        })}
     </Card>
   );
 }
